25d85e5c9501d1b1c601cc8f533a4e072ac9f800
chusniatul <48270037+chusniatul@users.noreply.github.com>
2019-04-09 15:36:56 +0700

    first commit

---
+++ database/migrations/2019_04_09_045009_create_table_menu.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTableMenu extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('menu', function (Blueprint $table) {
+            $table->integer('id_menu');
+            $table->string('Nama_menu');
+            $table->string('Harga');
+            $table->string('deskripsi');
+            $table->integer('stock');
+            $table->string('type');
+            $table->string('kategori');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('menu');
+    }
+}
